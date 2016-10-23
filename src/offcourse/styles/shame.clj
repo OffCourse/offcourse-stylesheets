531edfe9bb7bc4367b8abab411e19550417925fd
Zaturrby <rjkorteschiel@gmail.com>
2016-10-23 17:07:59 +0200

    add loading styles

--- src/offcourse/styles/shame.clj
+++ src/offcourse/styles/shame.clj
@@ -2,8 +2,15 @@
   (:refer-clojure :exclude [rem + - * /])
   (:require [garden
              [arithmetic :refer [*]]
-             [units :as u :refer [percent px rem]]
+             [units :as u :refer [percent px rem vw vh]]
              [stylesheet :refer [at-media]]]
             [offcourse.styles.vocabulary :as v]))
 
-(defn shame [{:keys [templates borders colors fonts units breakpoints]}])
+(defn shame [{:keys [templates borders colors fonts units breakpoints]}]
+  [[:.loading         (merge (:row-component     templates)
+                             {:align-items       :center
+                              :justify-content   :center
+                              :width             (vw 100)
+                              :height            (vh 100)})]
+   [:.loading--logo   (merge (:textbar           templates)
+                             {:padding          (:two-third units)})]])
