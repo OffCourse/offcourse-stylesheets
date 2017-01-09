d1d02587ab157d518823e4f3a386fce8201a7ecf
Zaturrby <rjkorteschiel@gmail.com>
2017-01-09 14:54:31 +0100

    header improvements

--- src/offcourse/styles/components/notifybar.clj
+++ src/offcourse/styles/components/notifybar.clj
@@ -14,20 +14,24 @@
                                      :background          (:dark colors)
                                      :color               (:day colors)})
      [(v/attr :data-notify-color :yellow)
-      (merge                        {:background (:yellow colors)
-                                     :color      (:night colors)})]
+      (merge                        {:background          (:yellow colors)
+                                     :color               (:night colors)})]
      [(v/attr :data-notify-color :blue)
-      (merge                        {:background (:blue colors)})]
+      (merge                        {:background          (:blue colors)})]
      [(v/attr :data-notify-color :red)
-      (merge                        {:background (:red colors)})]]
-   [:.notifybar--section     (merge (:row-component       templates)
+      (merge                        {:background          (:red colors)})]]
+   [:.notifybar--section     (merge (:row-component        templates)
                                     {:padding            [[0 (:full units)]]})]
 
-   [:.notifybar--subtitle    (merge (:subtitle            templates))]
-   [:.notifybar--text        (merge (:text                templates))]
-   [:.notifybar--link        (merge (:text                templates)
-                                    {:padding-left       (:third units)
-                                     :text-decoration     :underline})
-     [(v/attr :data-link-type :strong)
-      (merge                        (:subtitle            templates)
-                                    {:color              (:day colors)})]]])
+   [:.notifybar--subtitle    (merge (:subtitle             templates))]
+   [:.notifybar--text        (merge (:text                 templates))]
+   [:.notifybar--link        (merge (:text                 templates)
+                                    {:margin-left         (:third units)
+                                     :cursor               :pointer})
+    [v/hovered               (merge {:border-bottom      [["1px" "solid" (:night colors)]]
+                                     :margin-bottom        "-1px"})]
+    [(v/attr :data-link-type :strong)
+     (merge                         (:subtitle             templates)
+                                    {:color               (:day colors)})
+     [v/hovered              (merge {:border-bottom      [["1px" "solid" (:day colors)]]
+                                     :margin-bottom        "-1px"})]]]])
